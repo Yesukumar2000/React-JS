3b9e593e62074bd476228c1e3b6a2c5d6e9906f5
Yesukumar2000 <121677945+Yesukumar2000@users.noreply.github.com>
2024-06-06 19:45:49 +0530

    Frist-react-app creation changes

---
+++ Frist-React-App/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Testimonials</h1>
+    <div className="container">
+      <div className="box">
+        <img src='https://miro.medium.com/v2/resize:fit:720/format:webp/1*EyeBipMFitG-ZqvFclL9kA.jpeg' alt="Image1" />
+        <h2>Loori</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem eius porro eum officia accusantium distinctio! Assumenda beatae, nulla delectus nemo alias eligendi labore sed itaque totam ut. Tenetur, laudantium. Incidunt necessitatibus quibusdam magnam quo, unde cumque ratione molestias nemo quod iusto tempore cupiditate vel beatae at nostrum, perspiciatis obcaecati? Commodi.</p>
+      </div>
+      <div className="box">
+      <img src='https://www.massimofrancopsicoterapeuta.it/wp-content/uploads/2023/11/Mi-sento-sola-768x597.jpg' alt="Image2" />
+        <h2>Lilli</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem eius porro eum officia accusantium distinctio! Assumenda beatae, nulla delectus nemo alias eligendi labore sed itaque totam ut. Tenetur, laudantium. Incidunt necessitatibus quibusdam magnam quo, unde cumque ratione molestias nemo quod iusto tempore cupiditate vel beatae at nostrum, perspiciatis obcaecati? Commodi.</p>
+      </div>
+      <div className="box">
+      <img src='https://media.licdn.com/dms/image/D4D12AQFyPzzwwniaYg/article-cover_image-shrink_720_1280/0/1656051280558?e=1717027200&v=beta&t=O20-iK8wDK-Ra9Dnu9lllnCjMhlq0dekYwFHKkqZBL0' alt="Image3" />
+        <h2>Rohit</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem eius porro eum officia accusantium distinctio! Assumenda beatae, nulla delectus nemo alias eligendi labore sed itaque totam ut. Tenetur, laudantium. Incidunt necessitatibus quibusdam magnam quo, unde cumque ratione molestias nemo quod iusto tempore cupiditate vel beatae at nostrum, perspiciatis obcaecati? Commodi.</p>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
