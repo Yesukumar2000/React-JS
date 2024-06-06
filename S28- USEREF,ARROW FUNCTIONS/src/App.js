7807b5ac23bb11a1fc8588da586d8b6c82fe23d2
Yesukumar2000 <121677945+Yesukumar2000@users.noreply.github.com>
2024-06-06 20:31:01 +0530

    function types - Array functions in react js

---
+++ S28- USEREF,ARROW FUNCTIONS/src/App.js
@@ -0,0 +1,12 @@
+import TenthForm from './components/TenthForm.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <TenthForm />
+    </div>
+  );
+}
+
+export default App;
