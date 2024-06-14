2594001099dfe0dc32a3db20f426d88c3163a551
Yesukumar2000 <121677945+Yesukumar2000@users.noreply.github.com>
2024-06-14 11:51:28 +0530

    S51- String Methods

---
+++ S51- String Methods/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StringMethods from "./Components/StringMethods";
+
+function App() {
+  return (
+    <>
+      <StringMethods/>
+    </>
+  );
+}
+
+export default App;
